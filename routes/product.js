8c567f2c6e36202f3342c086e036c39576e468c7
Durgaprasad Singh <dp.9.singh@gmail.com>
2021-12-04 14:04:43 +0530

    Initial Commit

---
+++ routes/product.js
@@ -0,0 +1,66 @@
+const router = require('express').Router();
+const { verifyToken } = require("./verifyToken");
+const Product = require('../models/Product')
+
+// Add New Product
+router.post("/", verifyToken, async (req, res) => {
+  const newProduct = new Product(req.body);
+
+  try {
+    const savedProduct = await newProduct.save();
+    res.status(200).json(savedProduct);
+  } catch (err) {
+    res.status(500).json(err)
+  }
+});
+
+//UPDATE Product
+router.put("/:id", verifyToken, async (req, res) => {
+
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedProduct);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//DELETE Product
+router.delete("/:id", verifyToken, async (req, res) => {
+  try {
+    await Product.findByIdAndDelete(req.params.id);
+    res.status(200).json("Product has been deleted...");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//GET ALL PRODUCTS
+router.get("/", async (req, res) => {
+  const qCategory = req.query.category;
+  try {
+    let products;
+
+    if (qCategory) {
+      products = await Product.find({
+        categories: {
+          $in: [qCategory],
+        },
+      });
+    } else {
+      products = await Product.find();
+    }
+
+    res.status(200).json(products);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
